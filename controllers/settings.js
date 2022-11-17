c2e93a13d0ad6e94d96d2a34ac2144cc43e08261
matrixfoot <kammounomarindus@gmail.com>
2022-11-17 23:38:23 +0100

    carousel controller upated

--- controllers/settings.js
+++ controllers/settings.js
@@ -82,7 +82,7 @@ exports.updateCarouseldata = async (req, res, next) => {
         await Carousel.findByIdAndUpdate(_id, { ...carouselObject});
         
     carousel.updated = Date.now();
-    await (carousel.save(),sendupdateemail(carousel, origin)).
+    await (carousel.save()).
     then (()=> res.status(200).json({
       data: carousel,
       message: 'Actualitée modifiée!'
