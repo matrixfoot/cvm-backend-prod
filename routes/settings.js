d8d7c000faf2f500927b6d84e93e311361b40ba1
kammounomarindus@gmail.com <kammounomarindus@gmail.com>
2022-12-27 10:48:28 +0100

    route for settings updated

--- routes/settings.js
+++ routes/settings.js
@@ -4,7 +4,7 @@ const carouselController = require('../controllers/settings');
 const userController = require('../controllers/user');
 const multer = require('../middleware/multer-config');
 router.post('/createcarouseldata', multer,carouselController.createcarouseldata);
-router.get('/', userController.allowIfLoggedin, userController.grantAccess('readAny', 'carousel'), carouselController.getCarouselalldata);
+router.get('/', carouselController.getCarouselalldata);
 router.get('/:id', userController.allowIfLoggedin, userController.grantAccess('readOwn', 'carousel'),carouselController.getcarouseldatabyid);
 router.put('/:id', userController.allowIfLoggedin, multer ,userController.grantAccess('updateAny', 'carousel'),carouselController.updateCarouseldata);
 router.delete('/:id', userController.allowIfLoggedin, userController.grantAccess('updateAny', 'carousel'),carouselController.deleteCarouseldata);
