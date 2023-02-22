f21c42368db4d17cc67b8cf473b426f06e1f5a9d
matrixfoot <kammounomarindus@gmail.com>
2023-02-22 22:15:35 +0100

    sms feature updated2

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.use((req, res, next) => {
                 const mySender = 'TunSMS Test';
                /* const Url_str_accuse ="https://www.tunisiesms.tn/client/Api/Api.aspx?fct=dlr&key=8Xt1bBmrfe9Fuxj1tnAu9EXxNQmD9ilyxd2nzJ/ft5vUcv8d0FlnUbD/-/xkjFm6xYJgrZQib3Xq9c1qDuQfPIVaaOqRtTK9SD&msg_id=XXXX;YYYY";   */               
                 const Url_str ="https://www.tunisiesms.tn/client/Api/Api.aspx?fct=sms&key=8Xt1bBmrfe9Fuxj1tnAu9EXxNQmD9ilyxd2nzJ/ft5vUcv8d0FlnUbD/-/xkjFm6xYJgrZQib3Xq9c1qDuQfPIVaaOqRtTK9SD&mobile=216XXXXXXXX&sms=Hello+World&sender=YYYYYYY&date=jj/mm/aaaa&heure=hh:mm:ss";                  
-                const Url_str2 = Url_str.replace("Hello+World",`cher client${item.title}: dernier délai ${item.date.getFullYear()/item.date.getMonth() +1/item.date.getDate()} `);
+                const Url_str2 = Url_str.replace("Hello+World",`cher client ${item.title}: dernier délai ${item.date.getFullYear()/item.date.getMonth() +1/item.date.getDate()} `);
                 const Url_str3 = Url_str2.replace("YYYYYYY",mySender);
                 /*const Url_str_accuse1 = Url_str_accuse.replace("216XXXXXXXX",element.mobile)
                 const Url_str_accuse2 = Url_str_accuse1.replace("Hello+World",`veuillez noter que la date du ${item.date.split('T')[0]}est la date du ${item.title}`);
@@ -86,17 +86,23 @@ app.use((req, res, next) => {
                 const Url_str_accuse5 = Url_str_accuse4.replace("hh:mm:ss",myTime);
                 const finalurlaccuse=Url_str_accuse5*/
                 /*console.log(finalurlaccuse);*/
-                if(`${item.date.getDate()}`==currentdate.getDate() +4&&`${item.date.getMonth()}`==currentdate.getMonth()&&`${item.date.getFullYear()}`==currentdate.getFullYear())
+                if(`${item.date.getDate()}`==currentdate.getDate() +6&&`${item.date.getMonth()}`==currentdate.getMonth()&&`${item.date.getFullYear()}`==currentdate.getFullYear())
                 {
+                  console.log(item.nature.split(';'))
+                  console.log(item.natureactivite.split(';'))
+                  console.log(item.activite.split(';'))
+                  console.log(item.sousactivite.split(';'))
+                  console.log(item.regimefiscal.split(';'))
                   if(item.nature.split(';').find(x=>x==`${element.nature}`)||item.nature==''&&item.natureactivite.split(';').find(x=>x==`${element.natureactivite}`)||item.natureactivite==''
                     &&item.activite.split(';').find(x=>x==`${element.activite}`)||item.activite==''&&item.sousactivite.split(';').find(x=>x==`${element.sousactivite}`)||item.sousactivite==''&&
                     item.regimefiscal.split(';').find(x=>x==`${element.regimefiscalimpot}`)||item.regimefiscal=='')
                     {
+                     
                       const Url_str1 = Url_str3.replace("216XXXXXXXX",element.mobile)
                       const finalurl=Url_str1
                       const response = await fetch(finalurl);
                       /*const response2 = await fetch(finalurl);*/
-                      console.log('status code: ', response); // 👉️ 200
+                     console.log('status code: ', response.status); // 👉️ 200
                       if (!response.ok) {
                         throw new Error(`Error! status: ${response.status}`);
                       } 
